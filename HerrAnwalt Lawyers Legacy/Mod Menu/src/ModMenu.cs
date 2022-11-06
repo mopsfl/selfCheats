b3d76b2ff098f6f812dd6e6933169f0a973573ac
Flo <92175215+mopsfl@users.noreply.github.com>
2022-11-06 16:16:20 +0100

    Update ModMenu.cs
    New style, added drag

--- HerrAnwalt Lawyers Legacy/Mod Menu/src/ModMenu.cs
+++ HerrAnwalt Lawyers Legacy/Mod Menu/src/ModMenu.cs
@@ -7,16 +7,101 @@ namespace ModMenu
 	public class ModMenu : MonoBehaviour
 	{
 		// Token: 0x060004E1 RID: 1249
-		public static void DrawMenu()
+		static ModMenu()
+		{
+			ModMenu.MenuVisible = false;
+			ModMenu.speedhack = false;
+			ModMenu.speedhackString = "Speed Hack - OFF";
+			ModMenu.healString = "Max Health";
+			ModMenu.godModeString = "Infinite Health - OFF";
+			ModMenu.godMode = false;
+			ModMenu.ui = UnityEngine.Object.FindObjectOfType<AnwaltUIManager>();
+			ModMenu.infiniteJumpString = "Infinite Jump - OFF";
+			ModMenu.modmenuVersion = "0.2";
+			ModMenu.standardMaxHealth = ModMenu.ui.maxHealth;
+			ModMenu.Player = GameObject.FindGameObjectWithTag("Player");
+			ModMenu.plrControl = UnityEngine.Object.FindObjectOfType<PlayerControl>();
+			ModMenu.standardMovementSpeed = ModMenu.plrControl.movementSpeed;
+			ModMenu.standardJumpTimer = ModMenu.plrControl.jumpTimerSet;
+			ModMenu.Title = "HerrAnwalt: Lawyers Legacy Mod Menu v." + ModMenu.modmenuVersion;
+		}
+
+		// Token: 0x060004E2 RID: 1250
+		private static Texture2D MakeTex(int width, int height, Color col)
+		{
+			Color[] array = new Color[width * height];
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = col;
+			}
+			Texture2D texture2D = new Texture2D(width, height);
+			texture2D.SetPixels(array);
+			texture2D.Apply();
+			return texture2D;
+		}
+
+		// Token: 0x060004E3 RID: 1251
+		public static void DoMyWindow(int windowID)
 		{
 			GUIStyle guistyle = new GUIStyle(GUI.skin.label);
-			Texture2D background = new Texture2D(100, 30);
-			guistyle.normal.background = background;
-			guistyle.alignment = TextAnchor.MiddleCenter;
-			guistyle.normal.textColor = Color.black;
+			guistyle.normal.background = ModMenu.MakeTex(2, 2, new Color(50f, 0f, 0f, 1f));
+			guistyle.normal.textColor = Color.white;
+			guistyle.alignment = TextAnchor.UpperCenter;
+			guistyle.border = new RectOffset(2, 2, 2, 2);
+			if (GUI.Button(new Rect(10f, 30f, 380f, 20f), ModMenu.speedhackString, guistyle))
+			{
+				ModMenu.speedhack = !ModMenu.speedhack;
+				if (ModMenu.speedhack)
+				{
+					ModMenu.speedhackString = "Speed Hack - ON";
+					ModMenu.plrControl.movementSpeed = ModMenu.standardMovementSpeed * 3f;
+					return;
+				}
+				ModMenu.speedhackString = "Speed Hack - OFF";
+				ModMenu.plrControl.movementSpeed = ModMenu.standardMovementSpeed;
+			}
+			if (GUI.Button(new Rect(10f, 60f, 185f, 20f), ModMenu.godModeString, guistyle))
+			{
+				ModMenu.godMode = !ModMenu.godMode;
+				if (ModMenu.godMode)
+				{
+					ModMenu.godModeString = "Infinite Health - ON";
+					return;
+				}
+				ModMenu.godModeString = "Infinite Health - OFF";
+			}
+			if (!ModMenu.godMode && GUI.Button(new Rect(205f, 60f, 185f, 20f), ModMenu.healString, guistyle))
+			{
+				ModMenu.healString = "Max Health";
+				ModMenu.ui.healCompletly();
+			}
+			if (GUI.Button(new Rect(10f, 90f, 380f, 20f), ModMenu.infiniteJumpString, guistyle))
+			{
+				ModMenu.infiniteJump = !ModMenu.infiniteJump;
+				if (ModMenu.infiniteJump)
+				{
+					ModMenu.infiniteJumpString = "Infinite Jump - ON";
+					ModMenu.plrControl.jumpTimerSet = 10f;
+					ModMenu.plrControl.isGrounded = true;
+					return;
+				}
+				ModMenu.infiniteJumpString = "Infinite Jump - OFF";
+				ModMenu.plrControl.jumpTimerSet = ModMenu.standardJumpTimer;
+				ModMenu.plrControl.isGrounded = true;
+			}
+			GUI.DragWindow(new Rect(0f, 0f, 10000f, 20f));
+		}
+
+		// Token: 0x060004E4 RID: 1252
+		public static void LoadOldMenu()
+		{
+			GUIStyle guistyle = new GUIStyle(GUI.skin.label);
+			guistyle.normal.background = ModMenu.MakeTex(2, 2, new Color(0.1f, 0.1f, 0.1f, 1f));
+			guistyle.normal.textColor = Color.white;
+			guistyle.alignment = TextAnchor.UpperCenter;
+			guistyle.border = new RectOffset(2, 2, 2, 2);
 			if (Application.version != ModMenu.modmenuVersion)
 			{
-				Debug.Log("MOD MENU NOT COMPATIBLE FOR THIS GAME VERSION!");
 				GUI.Button(new Rect(0f, (float)(Screen.height - 30), (float)Screen.width, 20f), string.Concat(new string[]
 				{
 					"This Mod Menu version (",
@@ -35,8 +120,10 @@ namespace ModMenu
 			}
 			if (ModMenu.MenuVisible)
 			{
-				GUIStyle style = new GUIStyle(GUI.skin.box);
-				GUI.Box(new Rect(0f, 250f, 400f, 400f), ModMenu.Title, style);
+				GUIStyle guistyle2 = new GUIStyle(GUI.skin.box);
+				new Texture2D(100, 30);
+				guistyle2.normal.background = ModMenu.MakeTex(2, 2, new Color(0.1f, 0.1f, 0.1f, 1f));
+				GUI.Box(new Rect(0f, 250f, 400f, 400f), ModMenu.Title, guistyle2);
 				if (GUI.Button(new Rect(0f, 280f, 400f, 20f), ModMenu.speedhackString, guistyle))
 				{
 					ModMenu.speedhack = !ModMenu.speedhack;
@@ -81,7 +168,14 @@ namespace ModMenu
 			}
 			if (ModMenu.godMode)
 			{
-				ModMenu.ui.healCompletly();
+				if (ModMenu.ui)
+				{
+					ModMenu.ui.healCompletly();
+				}
+				else
+				{
+					UnityEngine.Object.FindObjectOfType<AnwaltUIManager>();
+				}
 			}
 			if (ModMenu.infiniteJump)
 			{
@@ -105,21 +199,66 @@ namespace ModMenu
 			}
 		}
 
-		// Token: 0x060004E3 RID: 1251
-		static ModMenu()
+		// Token: 0x060004E5 RID: 1253
+		public static void LoadMenu()
 		{
-			ModMenu.standardMaxHealth = ModMenu.ui.maxHealth;
-			ModMenu.Player = GameObject.FindGameObjectWithTag("Player");
-			ModMenu.plrControl = UnityEngine.Object.FindObjectOfType<PlayerControl>();
-			ModMenu.standardMovementSpeed = ModMenu.plrControl.movementSpeed;
-			ModMenu.standardJumpTimer = ModMenu.plrControl.jumpTimerSet;
+			GUIStyle guistyle = new GUIStyle(GUI.skin.label);
+			guistyle.normal.background = ModMenu.MakeTex(2, 2, new Color(0.1f, 0.1f, 0.1f, 1f));
+			guistyle.normal.textColor = Color.white;
+			guistyle.alignment = TextAnchor.UpperCenter;
+			guistyle.border = new RectOffset(2, 2, 2, 2);
+			if (Application.version != ModMenu.modmenuVersion)
+			{
+				GUI.Button(new Rect(0f, (float)(Screen.height - 30), (float)Screen.width, 20f), string.Concat(new string[]
+				{
+					"This Mod Menu version (",
+					ModMenu.modmenuVersion,
+					") is not compatible for this game version (",
+					Application.version,
+					")! Update to mod menu version ",
+					Application.version
+				}), guistyle);
+				return;
+			}
+			ModMenu.windowRect = GUI.Window(0, ModMenu.windowRect, new GUI.WindowFunction(ModMenu.DoMyWindow), ModMenu.Title, guistyle);
+			if (ModMenu.godMode)
+			{
+				if (ModMenu.ui)
+				{
+					ModMenu.ui.healCompletly();
+				}
+				else
+				{
+					UnityEngine.Object.FindObjectOfType<AnwaltUIManager>();
+				}
+			}
+			if (ModMenu.infiniteJump)
+			{
+				ModMenu.plrControl.isGrounded = true;
+			}
+			if (ModMenu.speedhack)
+			{
+				ModMenu.plrControl.movementSpeed = ModMenu.standardMovementSpeed * 3f;
+			}
+			if (!ModMenu.plrControl)
+			{
+				ModMenu.plrControl = UnityEngine.Object.FindObjectOfType<PlayerControl>();
+			}
+			if (!ModMenu.Player)
+			{
+				ModMenu.Player = GameObject.FindGameObjectWithTag("Player");
+			}
+			if (!ModMenu.ui)
+			{
+				ModMenu.ui = UnityEngine.Object.FindObjectOfType<AnwaltUIManager>();
+			}
 		}
 
 		// Token: 0x040004C7 RID: 1223
-		public static string Title = "HerrAnwalt: Lawyers Legacy Mod Menu - by mopsfl";
+		public static string Title;
 
 		// Token: 0x040004C8 RID: 1224
-		public static bool MenuVisible = false;
+		public static bool MenuVisible;
 
 		// Token: 0x040004C9 RID: 1225
 		public static PlayerControl plrControl;
@@ -131,39 +270,42 @@ namespace ModMenu
 		public static SprungBrett jumpPad;
 
 		// Token: 0x040004CC RID: 1228
-		public static bool speedhack = false;
+		public static bool speedhack;
 
 		// Token: 0x040004CD RID: 1229
-		public static string speedhackString = "Speed Hack - OFF";
+		public static string speedhackString;
 
 		// Token: 0x040004CE RID: 1230
 		public static GameObject Player;
 
 		// Token: 0x040004CF RID: 1231
-		public static string healString = "Max Health";
+		public static string healString;
 
 		// Token: 0x040004D0 RID: 1232
-		public static string godModeString = "Infinite Health - OFF";
+		public static string godModeString;
 
 		// Token: 0x040004D1 RID: 1233
-		public static bool godMode = false;
+		public static bool godMode;
 
 		// Token: 0x040004D2 RID: 1234
 		public static int standardMaxHealth;
 
 		// Token: 0x040004D3 RID: 1235
-		public static AnwaltUIManager ui = UnityEngine.Object.FindObjectOfType<AnwaltUIManager>();
+		public static AnwaltUIManager ui;
 
 		// Token: 0x040004D4 RID: 1236
 		public static bool infiniteJump;
 
 		// Token: 0x040004D5 RID: 1237
-		public static string infiniteJumpString = "Infinite Jump - OFF";
+		public static string infiniteJumpString;
 
 		// Token: 0x040004D6 RID: 1238
 		public static float standardJumpTimer;
 
 		// Token: 0x040004D7 RID: 1239
-		private static string modmenuVersion = "0.2";
+		private static string modmenuVersion;
+
+		// Token: 0x040004D8 RID: 1240
+		public static Rect windowRect = new Rect(20f, 250f, 400f, 400f);
 	}
 }
